38a8f9fa50e36aacb7f846092c58198a498c7b5f
Đăn Nam <dangnam97yt@gmail.com>
2023-05-24 17:50:15 +0700

    Search

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
-import "../style/Search.css"
+import React, { useState } from "react";
+import "../style/Search.css";
 
-const dataAPI = "https://raw.githubusercontent.com/sonnh7289/python3-download/main/location-data2.json?fbclid=IwAR1paTA_ucuBDjTSeiBK5wQM7tqmu9Ih6U-pxvb_t97Cp-QVPwAlDflEkNw"
- 
-function handlePost (){  
-   onclick = function(){
-        var name = document.querySelector('input[name="name"]').value;
-        console.log(name);
-    } 
+const dataAPI =
+  "https://raw.githubusercontent.com/sonnh7289/python3-download/main/location-data2.json?fbclid=IwAR1paTA_ucuBDjTSeiBK5wQM7tqmu9Ih6U-pxvb_t97Cp-QVPwAlDflEkNw";
+
+function handlePost() {
+  onclick = function () {
+    var name = document.querySelector('input[name="name"]').value;
+    console.log(name);
+  };
 }
-handlePost ()
+handlePost();
 
 // function postData(data, callback) {
 //     var options = {
 //         method : 'POST',
 //         headers: {
 //             "Content-Type": "application/json",
-         
+
 //           },
 //         body : JSON.stringify(data)
 //     };
@@ -27,54 +28,58 @@ handlePost ()
 //     })
 //     .then(callback);
 // }
- function Post() {
+function Post() {
   return (
     <div>
-       
-        <div>
-           
-            <input type="file" name='name'/>
-            <button onclick = "handlePost ()" id ='post'>Post</button>
-        </div>
-       
+      <div>
+        <input type="file" name="name" />
+        <button onclick="handlePost ()" id="post">
+          Post
+        </button>
+      </div>
     </div>
-  )
+  );
 }
-function inputInfor(){
-    var searchInput = document.querySelector('.search input')
-        searchInput.addEventListener('input', function(e) {
+function inputInfor() {
+  var searchInput = document.querySelector(".search input");
+  searchInput.addEventListener("input", function (e) {
     let txtSearch = e.target.value.trim().toLowerCase();
-    let listProductDOM = document.querySelectAll('.product');
-    listProductDOM.forEach(item =>{
-        if(item.innerText.toLowerCase().includes(txtSearch)){
-            item.classList.remove('hidde');
-        }else{
-            item.classList.add('hidd')
-        }
-    })
-})
-};
+    let listProductDOM = document.querySelectAll(".product");
+    listProductDOM.forEach((item) => {
+      if (item.innerText.toLowerCase().includes(txtSearch)) {
+        item.classList.remove("hidde");
+      } else {
+        item.classList.add("hidd");
+      }
+    });
+  });
+}
 // inputInfor();
 
-
- 
 function Search(props) {
-    return (
-    <div className='backg'>
-        <nav class=" container navbar d-flex ">
-            <div class="search">
-                <form className="d-flex">
-                    <button className='button'> <i class="fa-solid fa-magnifying-glass"></i></button>
-                    
-                    <input className="form-control" id='Search' type="search" placeholder="Tìm kiếm thứ bạn thích" aria-label="Search"/>
-                </form>
-            </div>
-            <Post/>
-        </nav>
+  return (
+    <div className="backg">
+      <nav class=" container navbar d-flex ">
+        <div class="search">
+          <form className="d-flex">
+            <button className="button">
+              {" "}
+              <i class="fa-solid fa-magnifying-glass p-1"></i>
+            </button>
+
+            <input
+              className="form-control"
+              id="Search"
+              type="search"
+              placeholder="Tìm kiếm thứ bạn thích"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+        <Post />
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Search;
-
-
