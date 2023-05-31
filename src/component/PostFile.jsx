96edfbb4ac47a4c458de14be0398d63f7a082c9d
Đăn Nam <dangnam97yt@gmail.com>
2023-05-31 11:29:59 +0700

    ima

--- src/component/PostFile.jsx
+++ src/component/PostFile.jsx
@@ -29,7 +29,7 @@ function FileUploadPage() {
           }
         );
         console.log(response);
-        setIsFilePicked(false);
+         setIsFilePicked(false)
       } catch (error) {
         console.error("Error:", error);
       }
